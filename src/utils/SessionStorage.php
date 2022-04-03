286b68b8681ff078855a374aff31828c25b5e744
R Givent A Simanjorang <a4smanjorg5@protonmail.com>
2022-04-03 08:59:00 +0700

    fix SessionStorage.php

--- src/utils/SessionStorage.php
+++ src/utils/SessionStorage.php
@@ -1,5 +1,17 @@
 <?php
-class SessionStorage implements IStorage
+
+/**
+ * This file is part of the utilsv package.
+ *
+ * (c) a4smanjorg5
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace a4smanjorg5\Utils;
+
+class SessionStorage implements \IStorage
 {
     private static $sess;
     private function __construct()
