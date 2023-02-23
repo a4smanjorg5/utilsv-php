a28f316a89b54ced3ecf9cc4fa176ee5baa7f46c
R Givent A Simanjorang <a4smanjorg5@protonmail.com>
2023-02-23 18:53:05 +0700

    add helpers

---
+++ obliged/helpers.php
@@ -0,0 +1,11 @@
+<?php
+function app_model($modelName, $newInstance = true) {
+    preg_match('/^[^\\\\]+/', $modelName, $m);
+    $model = 'App\\Models\\' . $m[0];
+    if ($m[0]) {
+        if ($newInstance)
+            return new $model;
+        else return $model;
+    }
+}
+?>
